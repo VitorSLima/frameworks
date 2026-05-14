8d26371253e867bf07361dc4a64c88b7f161d594
VitorSLima <dev.vitorlima@gmail.com>
2026-05-14 11:07:20 -0400

    feat: implement auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,27 @@
-// aqui é o controller
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { CurrentUser } from './current-user.decorator';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() data: RegisterDto) {
+    return await this.authService.register(data);
+  }
+
+  @Post('login')
+  async login(@Body() data: LoginDto) {
+    return await this.authService.login(data);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  me(@CurrentUser() user: { id: string; email: string }) {
+    return user;
+  }
+}
